19ccd7a1d0543323dee2bf4a81a99ee9225743c3
holliemaethomas <holliemaethomas85@gmail.com>
2019-11-19 20:15:28 -0800

    working random

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
- 'use strict';  
- 
- function Product (name, displayName) {
-   this.name = name
-   this.path = `img/${name}`
-   this.votes = 0
-   this.views = 0 
-   this.displayName = displayName
-   Product.all.push(this)
-   allImages.push(this.path)
-  }
-  var allImages = [];
-  var viewed = [];
-  var onDisplay = [];
-  Product.all = [];
-  Product.container = document.getElementById('imageContainer');
-  
-   var left = document.getElementById('left')
-   var middle = document.getElementById('middle')
-   var right = document.getElementById('right')
-  
-  // console.log(Product.all, 'I AM HERE!');
-  new Product('bag.jpg', 'bag')
-  new Product('banana.jpg', 'banana')
-  new Product('bathroom.jpg', 'bathroom')
-  new Product('boots.jpg', 'boots')
-  new Product('breakfast.jpg', 'breakfast')
-  new Product('bubblegum.jpg', 'bubblegum')
-  new Product('chair.jpg', 'chair')
-  new Product('cthulhu.jpg', 'cthulhu')
-  new Product('dog-duck.jpg', 'dog-duck')
-  new Product('dragon.jpg', 'dragon')
-  new Product('pen.jpg', 'pen')
-  new Product('pet-sweep.jpg', 'pet-sweep')
-  new Product('scissors.jpg', 'scissors')
-  new Product('sweep.jpg', 'sweep')
-  new Product('tauntaun.jpg', 'tauntaun')
-  new Product('unicorn.jpg', 'unicorn')
-  new Product('usb.jpg', 'usb')
-  new Product('water-can.jpg', 'water-can')
-  new Product('wine-glass.jpg', 'wine-glass')
-
-var renderProducts = function () {
-  
-    left.src = allImages[Math.floor(Math.random() * allImages.length)];
-    middle.src = allImages[Math.floor(Math.random() * allImages.length)];
-    right.src = allImages[Math.floor(Math.random() * allImages.length)];
-
-  } 
-  
-  
-  
+'use strict';
+//  globals
 
 
-renderProducts();
+// credit for shuffle function goes to (https://javascript.info/task/shuffle)
 
 
+Product.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+// DOM munipulation variables
+var left = document.getElementById('left')
+var middle = document.getElementById('middle')
+var right = document.getElementById('right')
+var leftName = document.getElementById('leftName')
+var rightName = document.getElementById('middleName')
+var leftName = document.getElementById('rightName')
+
+// arrays
+var randomizedArray = [];
+var allImages = [];
+var allnames = [];
+var viewed = [];
+var onDisplay = [];
+Product.all = [];
+Product.container = document.getElementById('imageContainer');
+
+function Product(name) {
+  this.name = name
+  this.path = `img/${name}.jpg`;
+  this.votes = 0
+  this.views = 0
+  Product.all.push(this)
+}
+for (var i = 0; i < Product.names.length; i++) {
+  new Product(Product.names[i])
+};
+
+function shuffle () {
+   return Product.all.sort(() => Math.random() - 0.5);
+}
+// credit to shuffle goes to: https://javascript.info/task/shuffle
+
+var current = [];
+function generateProduct () {
+  current[0] = shuffle()
+  current[1] = shuffle()
+  current[2] = shuffle()
+}
+
+generateProduct()
+console.log(current[0])
+
+var numArr = [1,2,3,4,5];
+
+var shuffledNumArr = [];
 
 
+function modifiedShuffle() {
+return numArr.sort(() => Math.random() - 0.5)
+};
 
+function assignRandomIndex () {
+  shuffledNumArr[0] = modifiedShuffle()
 
+}
 
-
+assignRandomIndex()
+console.log(shuffledNumArr)
